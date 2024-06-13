bb6d92e56662dd26c6bd43e109a420cdb4b52fda
ibrahimAlphaSquad <mibrahim.alphasquad@gmail.com>
2024-06-13 14:16:29 +0500

    changes

--- server.js
+++ server.js
@@ -174,5 +174,5 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port http://localhost:${PORT}`);
 });
